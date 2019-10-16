68c68950c6b30b46222f5e72eb30ac5bf9e6e8c4
sali1991 <sali.haguli@spotinst.com>
2019-10-16 14:35:56 +0300

    Merge branch 'master' into sali_branch_sdk_java
    # Conflicts:
    #	src/main/java/com/spotinst/sdkjava/example/ElastigroupUsageExample.java

--- src/main/java/com/spotinst/sdkjava/example/ElastigroupUsageExample.java
+++ src/main/java/com/spotinst/sdkjava/example/ElastigroupUsageExample.java
@@ -340,7 +340,7 @@ public class ElastigroupUsageExample {
         // Build elastigroup
         Elastigroup.Builder elastigroupBuilder = Elastigroup.Builder.get();
         Elastigroup elastigroup =
-                elastigroupBuilder.setName("sali_SpotinstTestGroup11111").setDescription("descriptive-information")
+                elastigroupBuilder.setName("SpotinstTestGroup").setDescription("descriptive-information")
                                   .setStrategy(strategy).setCapacity(capacity).setCompute(compute)
                                   .setThirdPartiesIntegration(thirdPartiesIntegration).setScheduling(scheduling)
                                   .build();
