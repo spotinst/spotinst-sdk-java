bfd289917e3631c6163eace66a08e28c1be31fa5
ohadmuchnik <ohad@spotinst.com>
2020-05-24 15:23:00 +0300

    DeleteRequestWithBody - Licensing modifications

--- src/main/java/com/spotinst/sdkjava/client/http/DeleteRequestWithBody.java
+++ src/main/java/com/spotinst/sdkjava/client/http/DeleteRequestWithBody.java
@@ -1,20 +1,17 @@
 package com.spotinst.sdkjava.client.http;
 
 import org.apache.http.annotation.NotThreadSafe;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
 
 import java.net.URI;
 
-/**
- * Created by aharontwizer on 8/9/15.
- *
-**/
 @NotThreadSafe
-public class DeleteRequestWithBody extends HttpEntityEnclosingRequestBase {
-    private static final String HTTP_METHOD_NAME = "DELETE";
+public class DeleteRequestWithBody extends HttpPost {
 
     @Override
-    public String getMethod() { return HTTP_METHOD_NAME; }
+    public String getMethod() {
+        return "DELETE";
+    }
 
     public DeleteRequestWithBody(final String requestUri) {
         super();
