2854dee7f6b4ac2bb0a743c372046d4a6f931faf
anuragsharma-123 <anurag.sharma@netapp.com>
2021-07-20 03:43:00 +0530

    aut-6155-list-all-accounts

--- src/main/java/com/spotinst/sdkjava/example/AccountUsageExampleAdmin.java
+++ src/main/java/com/spotinst/sdkjava/example/AccountUsageExampleAdmin.java
@@ -2,22 +2,46 @@ package com.spotinst.sdkjava.example;
 
 import com.spotinst.sdkjava.SpotinstClient;
 import com.spotinst.sdkjava.model.*;
-import com.spotinst.sdkjava.model.bl.admin.account.AccountDeleteRequest;
-
+import com.spotinst.sdkjava.model.bl.admin.account.*;
+import java.util.*;
 import java.io.IOException;
 
 public class AccountUsageExampleAdmin {
 
-    private final static String authToken = "your-token";
+    private final static String authToken = "";
 
     public static void main(String[] args) throws IOException {
         // Get account service client
         SpotinstAccountAdminClient spotinstAccountAdminClient = SpotinstClient.getAdminAccountClient(authToken);
 
-        // Delete account
-        String accountIdToDelete = "your-account-id-to-delete";
-        deleteAccount(spotinstAccountAdminClient, accountIdToDelete);
+        // -------------------- List all accounts Example ---------------------------
+        String yourCloudAccountId = "085875695241"; // This parameter can be Null, if all the accounts in the organization to be listed.
+        List<BlAccountAdmin> accountList = listAllAccounts(spotinstAccountAdminClient, yourCloudAccountId);
+
+        System.out.println("Total Accounts Found - " + accountList.size());
+
+        // For loop for iterating over the List
+        for (int i = 0; i < accountList.size(); i++) {
+            System.out.println("Account Id - " + accountList.get(i).getAccountId());
+            System.out.println("Organization Id - " + accountList.get(i).getOrganizationId());
+            System.out.println("Name - " + accountList.get(i).getName());
+            System.out.println("Cloud Provider - " + accountList.get(i).getCloudProvider());
+            System.out.println("Provider External Id - " + accountList.get(i).getProviderExternalId());
+        }
+
+        // -------------------- Delete account Example ---------------------------
+        // String accountIdToDelete = "your-account-id-to-delete";
+        //deleteAccount(spotinstAccountAdminClient, accountIdToDelete);
+
     }
+
+    private static List<BlAccountAdmin> listAllAccounts(SpotinstAccountAdminClient client, String cloudAccountId) {
+        ListAllAccountsRequest.Builder listAccountsBuilder = ListAllAccountsRequest.Builder.get();
+        ListAllAccountsRequest listAccountsRequest = listAccountsBuilder.setCloudAccountId(cloudAccountId).build();
+
+        return client.listAllAccounts(listAccountsRequest);
+    }
+
 
     private static void deleteAccount(SpotinstAccountAdminClient client, String accountIdToDelete) {
         AccountDeleteRequest.Builder deletionBuilder = AccountDeleteRequest.Builder.get();
--- src/main/java/com/spotinst/sdkjava/model/SpotAccountAdminService.java
+++ src/main/java/com/spotinst/sdkjava/model/SpotAccountAdminService.java
@@ -4,12 +4,52 @@ import com.spotinst.sdkjava.client.response.BaseServiceEmptyResponse;
 import com.spotinst.sdkjava.client.response.BaseSpotinstService;
 import com.spotinst.sdkjava.client.rest.*;
 import com.spotinst.sdkjava.exception.SpotinstHttpException;
+import com.spotinst.sdkjava.model.api.admin.account.ApiAccountAdmin;
 import org.apache.http.HttpStatus;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+
 public class SpotAccountAdminService extends BaseSpotinstService {
+
+    public static List<ApiAccountAdmin> listAccounts(String cloudAccountId, String authToken) throws SpotinstHttpException {
+
+        // Init retVal
+        List<ApiAccountAdmin> retVal = new LinkedList<>();
+
+        // Get endpoint
+        SpotinstHttpConfig config = SpotinstHttpContext.getInstance().getConfiguration();
+        String apiEndpoint = config.getEndpoint();
+
+        Map<String, String> queryParams = new HashMap<>();
+
+        // Add account Id Query param
+        if (cloudAccountId != null) {
+            queryParams.put("cloudAccountId", cloudAccountId);
+        }
+
+        // Get the headers for AWS.
+        Map<String, String> headers = buildHeaders(authToken);
+
+        //Build URI
+        String uri = String.format("%s/setup/account", apiEndpoint);
+
+        // Send the request.
+        RestResponse response = RestClient.sendGet(uri, headers, queryParams);
+
+        // Handle the response.
+        AccountAdminApiResponse allAccountsResponse = getCastedResponse(response, AccountAdminApiResponse.class);
+
+        if (allAccountsResponse.getResponse().getCount() > 0) {
+            retVal = allAccountsResponse.getResponse().getItems();
+        }
+
+        return retVal;
+    }
+
 
     public static Boolean deleteAccount(String accountId, String authToken) throws SpotinstHttpException {
 
