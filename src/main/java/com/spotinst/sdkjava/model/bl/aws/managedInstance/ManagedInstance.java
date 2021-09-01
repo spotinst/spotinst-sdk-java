12094c7510bfcd74d7cf4f9bb0bbbd9207017919
Chandra <nchandr1@netapp.com>
2021-09-01 13:13:09 +0530

    Updated ManagedInstance.java
    Removed IPartialUpdateEntity as it was not required

--- src/main/java/com/spotinst/sdkjava/model/bl/aws/managedInstance/ManagedInstance.java
+++ src/main/java/com/spotinst/sdkjava/model/bl/aws/managedInstance/ManagedInstance.java
@@ -4,15 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.spotinst.sdkjava.client.rest.IPartialUpdateEntity;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonFilter("PartialUpdateEntityFilter")
-public class ManagedInstance implements IPartialUpdateEntity {
+
+public class ManagedInstance {
 
     @JsonIgnore
     private Set<String>     isSet;
