33271d5112d38f76ef6e6751ac02f4730df96efd
Anurag Sharma <anurag4@netapp.com>
2023-10-18 21:28:26 +0530

    removed unused imports and bumped version

--- src/main/java/com/spotinst/sdkjava/model/ApiGroupCompute.java
+++ src/main/java/com/spotinst/sdkjava/model/ApiGroupCompute.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.spotinst.sdkjava.client.rest.IPartialUpdateEntity;
 import com.spotinst.sdkjava.model.api.elastigroup.aws.ApiElastigroupVolumeAttachments;
-import com.spotinst.sdkjava.model.bl.elastigroup.aws.ElastigroupVolumeAttachments;
 
 import java.util.HashSet;
 import java.util.List;
